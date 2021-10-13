7756009df9c7c3e1b7ed9e456ffe8340c6d8274d
chris terry <chris@chriss-MacBook-Pro.local>
2021-10-13 16:06:41 -0600

    mob next [ci-skip] [ci skip] [skip ci]

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,7 @@
 class Greeting
   def self.greet(name)
     name = name || 'my friend'
+    return "Hello, #{name.join ' and '}." if name.is_a? Array
     return "HELLO #{name}!" if capitalized?(name)
     "Hello, #{name}."
   end
