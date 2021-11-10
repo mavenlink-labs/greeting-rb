016ae3d9d8f1d8869bcfd79c2e3ef031f768907e
Ethan Anderson <ethanandy90@gmail.com>
2021-11-10 16:03:23 -0700

    mob next [ci-skip] [ci skip] [skip ci]

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,8 +1,8 @@
 class Greeting
   def self.greet(name)
-    uppercase_names, lowercase_names = [name].partition{ |n| n.upcase == n }
+    uppercase_names, lowercase_names = [name].flatten.partition{ |n| n.upcase == n }
 
-    lowercase_names.each { |l_name| return "Hello, #{l_name}." }
+    lowercase_names.each { |l_name| return "Hello, #{lowercase_names.join(' and ')}." }
     uppercase_names.each { |u_name| return "HELLO #{u_name}!" }
   end
 end
