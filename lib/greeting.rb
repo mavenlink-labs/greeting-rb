032ccea8e5240d9d063ddd77cc3708f7295db7bd
dezkowalski <dezkowalski@gmail.com>
2021-12-01 15:49:56 -0700

    mob next [ci-skip] [ci skip] [skip ci]

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,8 +2,13 @@ class Greeting
   def self.greet(name)
     uppercase_names, lowercase_names = [name].flatten.partition{ |n| n.upcase == n }
 
-    lowercase_names.each { |l_name| return "Hello, #{lowercase_names.join(' and ')}." }
-    uppercase_names.each { |u_name| return "HELLO #{u_name}!" }
+    # lowercase_names.each { |l_name| return "Hello, #{lowercase_names.join(' and ')}." }
+    # uppercase_names.each { |u_name| return "HELLO #{u_name}!" }
+    if(!lowercase_names.empty?)
+      "Hello, #{lowercase_names.join(' and ')}."
+    else
+      "HELLO #{uppercase_names.first}!"
+    end
   end
 end
 
